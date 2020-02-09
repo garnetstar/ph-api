809f82f79ef2ca243cc9abec4288653772ce0f0a
Jan Macháček <machacek.j@gmail.com>
2020-02-09 08:14:24 +0100

    add php 7.2 to build containder

---
+++ src/cli/build-and-push-deploy-image.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Bash3 Boilerplate. Copyright (c) 2014, kvz.io
+# https://kvz.io/blog/2013/11/21/bash-best-practices/
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+export IMAGE_NAME=pg-api-build:php7.2
+docker build -f ../../Docker/Dockerfile -t garnetstar/$IMAGE_NAME .
+docker login -u garnetstar docker.io
+docker push garnetstar/$IMAGE_NAME
+
