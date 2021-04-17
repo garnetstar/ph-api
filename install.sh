0d48290386693e5789aed1c38f747bf3bde431c7
Jan Macháček <jan.machacek@kosik.cz>
2021-04-17 14:37:25 +0200

    add database

--- install.sh
+++ install.sh
@@ -9,5 +9,14 @@ docker run -it -v $(pwd)/src/:/api/src/ pn-api:composer bash -c 'cd src && compo
 # build prod image
 docker build --target prod -t pn-api:v1 -f Docker/Dockerfile .  
 
+# network 
+docker network create pnet  
+
+# database
+docker run  --network pnet -p 3306:3306 --rm -v $(pwd)/db/:/db/ -d --name db -e MYSQL_ROOT_PASSWORD=xxx -e MYSQL_DATABASE=pn mariadb:10.2.26 
+docker exec -it db bash -c "mysql -u root -pxxx < /db/init.sql"  
+
 #run prod image
-docker run -d --rm --name pn-api -p 88:80 pn-api:v1 
+docker run --network pnet -e DB_PASSWORD=xxx -e DB_USER=root -e DB_HOST=db -e DB_NAME=pn -d --rm --name pn-api -p 88:80 pn-api:v1 
+
+
